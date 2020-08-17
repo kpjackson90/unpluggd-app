e2766fd7605db66522d832e9878ad60fbee1e6fd
Sofiyullah jamiu <sofiyullahj@gmail.com>
2020-08-17 18:11:42 +0100

    signup page setup

---
+++ client/pages/signup.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Forminput from "./utils/FormInput";
+import { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const bg = {
+    backgroundImage: `url('/images/image-back@3x.png')`,
+  };
+
+  return (
+    <div className="app-height signup" style={bg}>
+      <div className="row">
+        <div className="col-6">
+          <img src="/images/image-left@3x.png" className="image-left" />
+        </div>
+        <div className="col-6 d-flex align-items-center justify-content-center">
+          <div className="max-345 ml-5 mr-auto text-center">
+            <img src="/images/logo@3x.png" alt="" className="signup-logo" />
+            <h4 className="title-h4">Sign Up</h4>
+
+            <button className="fb-btn app-btn">
+              <span className="f-24 mr-4">
+                <i class="fab fa-facebook-f"></i>
+              </span>
+              <span>Sign Up with Facebook</span>
+            </button>
+            <button className="ig-btn app-btn">
+              <span className="f-24 mr-4">
+                <i class="fab fa-instagram"></i>
+              </span>
+              <span>Sign Up with Instagram</span>
+            </button>
+            <div className="line-break">
+              <div></div>
+              <span className="f-16 fw-bold ml-2 mr-2">OR</span>
+              <div></div>
+            </div>
+            <form>
+              <Forminput
+                placeholder="Email Address"
+                type="text"
+                value={email}
+                setValue={setEmail}
+                invalid={true}
+                textInfo="Email address invalid"
+              />
+              <Forminput
+                placeholder="Password"
+                type="password"
+                value={password}
+                setValue={setPassword}
+              />
+              <Forminput
+                placeholder="Confirm Password"
+                type="password"
+                value={confirmPassword}
+                setValue={setConfirmPassword}
+                valid={true}
+                textInfo="Password Match"
+              />
+              <button className="app-btn signup-btn bg-teal">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
---
+++ client/pages/utils/FormInput.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Forminput = (props) => {
+  const {
+    placeholder,
+    value,
+    setValue,
+    type,
+    valid,
+    invalid,
+    textInfo,
+  } = props;
+
+  return (
+    <div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="app-input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        style={{
+          borderColor: valid ? "#007A87" : invalid ? "#E72287" : "#464646",
+        }}
+      />
+      {textInfo ? (
+        <small
+          className="form-text fw-400 f-16 text-left"
+          style={{ color: valid ? "#12ccc7" : invalid ? "#E72287" : "initial" }}
+        >
+          {textInfo}
+        </small>
+      ) : null}
+    </div>
+  );
+};
+
+export default Forminput;
