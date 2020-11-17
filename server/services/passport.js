26c9fb22dda12b551b16873c8865d33b39f26439
Kareem Phillip-Jackson <kareemphillip-jackson@Kareems-MacBook-Pro.local>
2020-11-16 21:34:31 -0400

    dockerize application

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,9 @@
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook');
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('User');
+const User = mongoose.model("User");
 
 // passport.serializeUser((user, done) => {
 // 	done(null, user.id);
@@ -16,28 +16,34 @@ const User = mongoose.model('User');
 // });
 
 passport.use(
-	new FacebookStrategy(
-		{
-			clientID: keys.FACEBOOK_CLIENT,
-			clientSecret: keys.FACEBOOK_SECRET,
-			callbackURL: '/api/user/facebook/callback',
-			profileFields: ['id', 'displayName', 'email'],
-			proxy: true,
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			const role = 'user';
-			const isVerified = true;
+  new FacebookStrategy(
+    {
+      clientID: "738261756927467",
+      clientSecret: "32f1f713c2907edf5057d1a2439bf6a2",
+      callbackURL: "/api/user/facebook/callback",
+      profileFields: ["id", "displayName", "email"],
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const role = "user";
+      const isVerified = true;
 
-			const {id, name, email} = profile._json;
+      const { id, name, email } = profile._json;
 
-			const existingUser = await User.findOne({facebook: id});
+      const existingUser = await User.findOne({ facebook: id });
 
-			if (existingUser) {
-				return done(null, existingUser);
-			}
+      if (existingUser) {
+        return done(null, existingUser);
+      }
 
-			const newUser = await new User({email, role, username: name, isVerified, facebook: id}).save();
-			done(null, newUser);
-		}
-	)
+      const newUser = await new User({
+        email,
+        role,
+        username: name,
+        isVerified,
+        facebook: id,
+      }).save();
+      done(null, newUser);
+    }
+  )
 );
