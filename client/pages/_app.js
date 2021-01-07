c5aa710e122014b377d86419f65888319e45c128
Kareem Phillip-Jackson <kareemphillip-jackson@Kareems-MacBook-Pro.local>
2021-01-07 18:56:00 -0400

    clean up _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "../styles/styles.css";
 import React from "react";
-import Api from '../api/build-client';
 import { Provider as AuthProvider } from '../context/AuthContext';
 
 
@@ -14,26 +13,5 @@ const AppComponent = ({ Component, pageProps}) => {
       </AuthProvider>
   );
 }
-
-// AppComponent.getInitialProps = async (appContext) => {
-//   const client = Api(appContext.ctx);
-//   const { data } = await client.get('/api/user/me');
-
-//   let pageProps = {};
-//   if (appContext.Component.getInitialProps) {
-//     pageProps = await appContext.Component.getInitialProps(
-//       appContext.ctx,
-//       client,
-//       data?.existingUser
-//     )
-//   }
-
-//   return {
-//     pageProps,
-//     ...data
-//   }
-// }
-
-
 
 export default AppComponent;
