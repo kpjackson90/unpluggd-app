5d65a1798fee0ce6df4859dfecfa7fd312823204
freemile2ru <sofiyullahj@gmail.com>
2021-02-02 14:59:37 +0100

    updated exit-event page

--- client/pages/attend/exit-event.jsx
+++ client/pages/attend/exit-event.jsx
@@ -17,9 +17,9 @@ const ExitEvent = () => {
       <div className="position-relative text-center">
         <img src="/images/Logo@3x.png" className="max-155 mb-24" />
         <p className="f-24 fw-400 mb-36">Logout</p>
-        <p className="f-16 fw-300 mb-36">
-          Are you sure you want to log out? You will <br /> need to manually log
-          back in to manage or <br /> review your events.
+        <p className="f-16 fw-300 mb-36 text-left">
+          Are you sure you want to leave the event? <br /> You can still hop
+          back into the event room <br /> before the event ends in 10 mins.
         </p>
         <div className="max-254 ml-auto mr-auto">
           <Link href="/attendee/dashboard">
@@ -28,7 +28,7 @@ const ExitEvent = () => {
             </button>
           </Link>
           <button
-            className="btn-transparent w-100 gray fw-500"
+            className="btn-transparent w-100 gray text-white fw-500"
             onClick={() => router.back()}
           >
             No, maybe later
